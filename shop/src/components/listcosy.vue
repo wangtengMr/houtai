829e641c3abf93c9322929fbc10a0a75634904e0
wangyi <1209187811@qq.com>
2023-04-06 20:06:36 +0800

    created cossyi

---
+++ shop/src/components/listcosy.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        用户分类
+    </div>
+</template>
+
+<!-- <script>
+import {ref, reactive, toRefs} from 'vue'
+import { useStore } from 'vuex';
+import { useRoute, useRouter } from 'vue-router';
+export default {
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({
+
+    })
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style> -->
