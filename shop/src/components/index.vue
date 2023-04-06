d17d386a80b285908b044dfc20a56e02973e2f91
wangyi <1209187811@qq.com>
2023-04-06 19:56:40 +0800

    created list

---
+++ shop/src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        用户首页
+    </div>
+</template>
+
+<!-- <script>
+import {ref, reactive, toRefs} from 'vue'
+import { useStore } from 'vuex';
+import { useRoute, useRouter } from 'vue-router';
+export default {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({
+
+    })
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style> -->
