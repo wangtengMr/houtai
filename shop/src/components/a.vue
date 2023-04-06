1147506aa156f4a0f18e976d5807fd485ac7697a
wangyi <1209187811@qq.com>
2023-04-06 20:21:11 +0800

    created a

---
+++ shop/src/components/a.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        张豪是个狗八
+    </div>
+</template>
+
+<!-- <script>
+import {ref, reactive, toRefs} from 'vue'
+import { useStore } from 'vuex';
+import { useRoute, useRouter } from 'vue-router';
+export default {
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+    const data = reactive({
+
+    })
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style> -->
