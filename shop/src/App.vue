ae4ef221698918f532ed4b44509c934452b17378
ZhangHao <3557683235@qq.com>
2023-04-06 14:10:36 +0800

    删除文字

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -5,7 +5,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    王腾我是你爹
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
